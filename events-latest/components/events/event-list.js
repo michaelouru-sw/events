b9fe57d4c8e2ed39c42843293e13c80b69018578
Michael Ouru <39490502+michaelouru-sw@users.noreply.github.com>
2023-06-12 15:26:38 +0300

    Add components

---
+++ events-latest/components/events/event-list.js
@@ -0,0 +1,19 @@
+import classes from "./event-list.module.css";
+import EventItem from "./event-item";
+export default function EventList(props) {
+  const { items } = props;
+  return (
+    <ul className={classes.list}>
+      {items.map((event) => (
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
+      ))}
+    </ul>
+  );
+}
--- events-latest/pages/index.js
+++ events-latest/pages/index.js
@@ -1,7 +1,11 @@
+import EventList from "@/components/events/event-list";
+import { getFeaturedEvents } from "@/dummy-data";
+
 export default function Home() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>This is the home page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
