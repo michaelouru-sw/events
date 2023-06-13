34bd174a34ef1cec77b60a614569aca9f20f95f9
Michael Ouru <39490502+michaelouru-sw@users.noreply.github.com>
2023-06-13 22:13:51 +0300

    complete the search slug

--- events-latest/pages/events/[...slug].js
+++ events-latest/pages/events/[...slug].js
@@ -1,7 +1,52 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "@/dummy-data";
+import EventList from "@/components/events/event-list";
+import EventSearch from "@/components/events/events-search";
+import ResultsTitle from "@/components/events/results-title";
+
 export default function AllEvents() {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
+
+  if (!filterData) {
+    return <p className="center">Loading</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filterMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filterMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents === 0) {
+    return <p>No events found for the chosen filter</p>;
+  }
+  const titleDate = new Date(numYear, numMonth - 1);
+
   return (
     <div>
-      <h1>All events that meet a certain criteria will appear here</h1>
+      <ResultsTitle date={titleDate} />
+      <EventList items={filteredEvents} />
     </div>
   );
 }
--- events-latest/pages/events/index.js
+++ events-latest/pages/events/index.js
@@ -1,10 +1,19 @@
 import EventList from "@/components/events/event-list";
 import { getAllEvents } from "@/dummy-data";
+import EventSearch from "@/components/events/events-search";
+import { useRouter } from "next/router";
 
 export default function Events() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
   return (
     <div>
+      <EventSearch onSearch={findEventHandler} />
       <EventList items={events} />
     </div>
   );
